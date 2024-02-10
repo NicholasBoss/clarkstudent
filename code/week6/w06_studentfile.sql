c38629a2eb887ab3f35e88e15a40c8f95eff1cf8
NicholasBoss <NicholasBoss0914@gmail.com>
2024-02-10 15:13:09 -0700

    added template examples for UPDATE and DELETE statements.

--- code/week6/w06_studentfile.sql
+++ code/week6/w06_studentfile.sql
@@ -58,51 +58,24 @@
 -- U
 -- ***
 
+/*
+  Here is an example template for the UPDATE statement:
+  UPDATE tablename
+  SET    column_name = 'string value'
+  WHERE  table_primary_key = 1;
+*/
+
  -- 7. Update the calling of Doris to Relief Society President
 
 -- ***
 -- D
 -- ***
 
+/*
+  Here is an example template for the DELETE statement:
+  DELETE FROM tablename
+  WHERE  table_primary_key = 1;
+*/
 -- 8. Delete the records of Jared MacWilliamns and Doris Garcia
 
-
-
--- _________________________________________________________________
--- TODO: add update structure example similar to Insert
--- TODO: add delete strucuture example similar to Insert
--- TODO: remove this TODO text and all text below
--- _________________________________________________________________
-
--- This is VERY DANGEROUS and will change all actors to 'Tommy'
--- If safe mode is enabled it could prevent it but don't plan on that
-
--- This only updates Tommy Hanks from 'Tom Hanks' to 'Tommy Hanks'
-
--- Better to use primary key when updating or deleting
-
--- Delete the record/entry of the actor with the ID of 1
-
--- DELETE the entire table and all entries/records in the 'actors' table
-
--- DELETE the entire database, all tables, and all entries/records
-
--- The faster way to do inserts, create all records in one insert statement
--- Run after you forward engineer an empty database
--- Creates new records/rows for each actor
-
--- Better to use the VALUE column names 
--- INSERT INTO actors VALUES (actor_id, last_name, first_name)
-
--- Output the result set of all columns (*) from the table 'actors'
-
--- Create records of the movies
-
-
--- Output the result set of all columns (*) from the table 'movies'
-
-
--- Inserting data into a linking table
--- Look at SELECT statements for both actor and movie to get the ID numbers here
--- Look at the PDF or dataset provided to know which actor is in which movie
 
