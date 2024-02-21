0fe64de2d9a26dc1eff39dbaddbb4343818e8c03
NicholasBoss <NicholasBoss0914@gmail.com>
2024-02-21 08:39:14 -0700

    Updated week 7 workbook

--- code/week7/w07_studentfile.sql
+++ code/week7/w07_studentfile.sql
@@ -2,12 +2,22 @@
 -- W07 STUDENT SQL WORKBOOK
 -- *********************************
 
--- Read (SELECT) may be all we need for this week
+/*
+     SELECT   column_name AS 'Alias1'
+     ,        column_name_2 AS 'Alias2'
+     FROM     table_name
+     WHERE    column_name = condition
+     ORDER BY column_name (DESC)
+     LIMIT    # of rows;
+*/
+
+USE world;
+-- For questions 2 through 5, limit the records returned to 5.
 -- 1. SELECT all columns from the country table
 -- 2. SELECT the name, population, and continent from the country table
--- 3. Filter the previous query by populations bigger than 800,000
--- 4. Switch the filter to only show countries from the Americas
--- 5. ORDER BY the population from greatest to least
+-- 3. Filter the previous query by populations bigger than 800,000 (>)
+-- 4. Switch the filter to only show countries from the Americas (OR/IN)
+-- 5. ORDER BY the population from greatest to least (DESC)
 -- 6. Switch the ORDER BY to use continent, then name
 -- 7. What is DESC doing? What do we do to show the results in alphabetical order?
 -- 8. Do the same queries as above without DESC. What do we find as the smallest
@@ -15,10 +25,18 @@
 -- 9. Filter the query by population between 400,000 and 500,000
 -- 10. ORDER BY population
 
+-- *******************************
+-- LIKE Operator (WHERE clause)
+-- *******************************
+
 -- 11. Select all city names that begin with 'Las'
 -- 12. Add the CountryCode to tell which country those cities reside in
 -- 13. SELECT all city names that end in 'gre'
 -- 14. SELECT all city names with 'end' somewhere in them
+
+-- *******************************
+-- IS NULL/IS NOT NULL Operators
+-- *******************************
 
 -- 15. SELECT all country names, lifeExpectancy, and continent
 --     WHERE LifeExpectancy IS NULL
@@ -30,101 +48,27 @@
 -- 20. Modify the previous query to find the LifeExpectancy of Germany and the US.
 --     If we remove the ORDER BY statement, does it affect the query?
 
+-- *******************************
+-- Calculations in SELECT clause
+-- *******************************
 
--- TODO change to WORLD DB - DONE
--- TODO add in DISTINCT queries
--- TODO Add the use of AND in the queries
--- TODO REGEXP?
--- TODO show aliases?
--- TODO Use the Bike database to show calculations?
+USE bike;
 
-
-
-
--- Outputs all columns from the table 'artwork'
-
--- Show me the first three pieces of artwork (title and period)
---  in alphabetical order
-
-
--- Show me all the different periods from the artwork 
--- (DISTINCT, only one of each - gets rid of repeats)
-
-
--- Show me the artwork from the artist with id #3
-
-
--- Show me artwork with the title's that start with A to I
-     -- try < 'J' to get I's
--- What about Ia or I to Z
-
--- Show me the title of the artwork for the years 1800 to 1904
-SELECT title, artyear
-FROM artwork
-WHERE artyear > 1800 AND artyear < 1904;
--- OR --- BETWEEN IS INCLUSIVE - uses the first and last number
-
--- Show me the artwork titles for the periods 'Modern' and 'Baroque'
-
--- Show me the titles and periods for the artwork that has the period having part of the word 'impr'
-
--- OR 
--- Use REGEXP now
-SELECT title, period
-FROM artwork
-WHERE period REGEXP 'impr';
-
--- Show me the title and period of the artwork that begins with word 'Post'
-SELECT title, period
-FROM artwork
-WHERE period REGEXP '^Post';
-
--- Show me the title and period of the artwork that ends with 'ism' for period
-SELECT title, period
-FROM artwork
-WHERE period REGEXP 'ism$';
-
--- Show me the title of the artwork that begins with the word 'Old' (title)
-SELECT title 
-FROM artwork
-WHERE title REGEXP '^Old';
-
--- Show me the title and period of the artwork that has 'the' 'in' or 'on' in the title
-SELECT title
-FROM artwork
-WHERE title REGEXP 'the|in|on';
-
--- Show me the full name of an artist for those who have middle names and sort it by an Alias called Middle
-
--- can't use Middle for WHERE
-
--- From the bike database
-
-
--- Show me all of the records in the product table
-
-
--- Show me products, model years, and list price but display list price as the column 'Price' 
--- without editing the database and from the most expensive to least expensive 
- SELECT product_name, model_year, list_price AS Price
- FROM product
- ORDER by Price;
-
--- Show me products, model years, and list price 
--- but round it and display the column as 'Rounded Price' without editing the database
-SELECT product_name, model_year, ROUND(list_price) AS 'Rounded Price'
-FROM product;
- 
 -- Show me the products, model years, and list price and add $100 to each without editing the database 
 SELECT product_name, model_year, list_price, list_price + 100
 FROM product;
 
 -- Show me products, model years, and list price and list the column as "price_adjustment"
 -- and show me only the prices over $1000
-SELECT product_name, list_price + 100 AS price_adjustment
+-- Round to 2 decimal places
+SELECT product_name, ROUND(list_price + 100) AS price_adjustment
 FROM product
 WHERE list_price + 100 > 1000   -- can't use price_adjustment here because of Order of Executuion
 ORDER BY price_adjustment;     -- can use it here however
+
+-- *******************************
+-- Nested OR/AND Operators
+-- *******************************
 
 -- Show me the products and model year of the products that start with Trek or Surly
 -- (but not from 2016)
@@ -137,3 +81,29 @@ WHERE (product_name LIKE 'Trek%' OR product_name LIKE 'Surly%') AND NOT model_ye
 SELECT product_name, model_year
 FROM product
 WHERE (product_name REGEXP '^Trek' OR product_name REGEXP '^Surly') AND model_year <> 2016;
+
+
+-- *******************************
+-- DISTINCT Operator
+-- *******************************
+
+USE v_art;
+
+-- Show me all the different periods from the artwork 
+-- (DISTINCT, only one of each - gets rid of repeats)
+SELECT DISTINCT period
+FROM   artwork;
+
+-- *******************************
+-- REGEXP Examples
+-- *******************************
+
+-- Selects any period with 'impr' anywhere in the name of the period
+SELECT title, period
+FROM artwork
+WHERE period REGEXP 'impr';
+
+-- Show me the title and period of the artwork that has 'the', 'in', or 'on' in the title
+SELECT title
+FROM artwork
+WHERE title REGEXP 'the|in|on';
