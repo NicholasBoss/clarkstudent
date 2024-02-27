a371aeedac412875865f956ee7cf2ec4a61585ff
NicholasBoss <NicholasBoss0914@gmail.com>
2024-02-27 14:31:53 -0700

    updating week9 workbook

--- code/week9/w09_studentfile.sql
+++ code/week9/w09_studentfile.sql
@@ -1,11 +1,37 @@
+-- *********************************
+-- W09 STUDENT SQL WORKBOOK
+-- *********************************
+
+/*
+     SELECT     column_name AS 'Alias1'
+     ,          Function(column_name_2) AS 'Alias2'
+     FROM       table1 t1
+     INNER (LEFT/RIGHT) JOIN table2 t2
+     ON         t1.table1_id = t2.table2_id
+     WHERE      column_name = condition
+     ORDER BY   column_name (DESC)
+     LIMIT      # of rows;
+     To remember this: Stay Firm With Our Lord
+*/
+
+-- *******************************
+-- INNER JOIN
+-- *******************************
+
+
+-- *******************************
+-- LEFT/RIGHT JOIN
+-- *******************************
+
 -- Use the v_art database
 
 -- Look at artist_id in the artist table
 
 -- Look at artist_id in the artwork table
 
--- A list of all the artists with each of their art works (can demonstrate alias's and USING if you'd like here, but realize the USING clause isn't universal for all RDBMS and there are some limitations with USING. That is why we teach with ON not USING) 
- 
+
+-- A list of all the artists with each of their art works (can demonstrate alias's and USING() if you'd like here, but realize the USING clause isn't universal for all RDBMS and there are some limitations with USING. That is why we teach with ON not USING) 
+
  -- We specify INNER JOIN and not just JOIN to allow for cross server compatability (it's industry standard as well to specify which join).
  -- these are the two IDs (usually the same) from two tables you are merging
 
@@ -15,7 +41,6 @@
  -- adding a table alias for artwork called 'aw'
  -- using a. meaning the table artist and aw. for the table artwork
 
--- Maybe Show them art.ceiwebdev.com
 
 -- What if I only want Leonardo diVinci's?
  -- OR fname = 'Leonardo'
@@ -33,9 +58,9 @@
 -- Output 'product' table
 
 -- Output products, categories, and list prices
-   
+
 -- I also want the brand as well, this involves 3 tables
-    
+
 -- On your own: All children category bikes
 
 -- On your own: All staff at Rowlett Bikes Store
@@ -58,12 +83,10 @@
 -- and only the related data of the subscription table? (On their own)
 -- list out magazineName, magazinePrice, and subscriptionStartDate (use table aliases)
 
-    
 -- Now add to the query above the 3rd table of subscribers so you can see
 -- their last name (demonstrates using 3 tables in a OUTER JOIN) (On their Own)
 -- It should still show the one magazine with  date and name as null
 
-    
 -- need the second JOIN as LEFT otherwise you don't get all the results from 
 -- the first join that includes the one magazine with no relations.
 -- Because that magazine didn't have a related magazineKey in the first JOIN, it won't
@@ -71,7 +94,7 @@
 -- A Venn diagram can explain this with 3 circles. 
 
 -- FULL OUTER JOIN simulated with UNION in MySQL
--- Copy the LEFT OUTER JOIN above and UNION it to Same with ithe RIGHT JOINS
+-- Copy the LEFT OUTER JOIN above and UNION it to Same with the RIGHT JOINS
 SELECT magazineName, magazinePrice, subscriptionStartDate, subscriberLastName
 FROM magazine m 
 	LEFT JOIN subscription s
