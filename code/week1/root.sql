3b36330a4e27571f18cad59b804c40dc9e0bdcfc
Nicholas Boss <86071846+NicholasBoss@users.noreply.github.com>
2024-03-22 08:17:28 -0600

    Updated the root.sql and root.py files

--- code/week1/root.sql
+++ code/week1/root.sql
@@ -1,10 +1,30 @@
+-- ~
+CREATE USER IF NOT EXISTS 'student'@'localhost' IDENTIFIED BY 'student';
+-- ~
+-- ~
 GRANT ALL ON bike.* TO 'student'@'localhost';
--- Replace 'bike' with the names of the following databases and run each one:
--- employees
--- magazine
--- v_art
--- world
--- datawinter
--- church
--- film
--- university
+-- ~
+-- ~
+GRANT ALL ON employees.* TO 'student'@'localhost';
+-- ~
+-- ~
+GRANT ALL ON magazine.* TO 'student'@'localhost';
+-- ~
+-- ~
+GRANT ALL ON v_art.* TO 'student'@'localhost';
+-- ~
+-- ~
+GRANT ALL ON world.* TO 'student'@'localhost';
+-- ~
+-- ~
+GRANT ALL ON datawinter.* TO 'student'@'localhost';
+-- ~
+-- ~
+GRANT ALL ON church.* TO 'student'@'localhost';
+-- ~
+-- ~
+GRANT ALL ON film.* TO 'student'@'localhost';
+-- ~
+-- ~
+GRANT ALL ON university.* TO 'student'@'localhost';
+-- ~
